044d6ab940c2a4c5e87ea26f9df9ba3506c040ed
Ankush-Kibbcom <ankush.gattewar@kibbcom.in>
2017-08-30 14:55:44 +0530

    Update token.php

--- token.php
+++ token.php
@@ -1,4 +1,10 @@
- <?php
+<?php
+define(‘SECRET_KEY’,’Key’) /// secret key can be any random string
+
+$tokenId = base64_encode(mcrypt_create_iv(32));
+$issuedAt = time();
+$notBefore = $issuedAt + 10; //Adding 10 seconds
+$expire = $notBefore + 7200; // Adding 60 seconds
   $header = [
            'typ' => 'JWT',
 		   'alg' => 'HS256'
@@ -12,13 +18,18 @@ $payload = [
 		"country" => "Romania",
 		"name" => "Octavia-Anghel",
 		"email" => "octaviaanghel@gmail.com"
+		‘iat’ => $issuedAt, // Issued at: time when the token was generated
+		‘jti’ => $tokenId, // Json Token Id: an unique identifier for the token
+		‘iss’ => $serverName, // Issuer
+		‘nbf’ => $notBefore, // Not before
+		‘exp’ => $expire, // Expire
 		  ];
-
+		  
 $payload = json_encode($header);		
 $payload = base64_encode($header);
 
 // Generates a keyed hash value using the HMAC method
-$signature = hash_hmac('sha256','$header.$payload', $key, true);
+$signature = hash_hmac('sha256','$header.$payload', SECRET_KEY, true);
 
 //base64 encode the signature
 $signature = base64_encode($signature);
